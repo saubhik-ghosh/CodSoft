1c940533af9227a4cd2afe4888e42a9cc03f8c2c
Saubhik Ghosh <76199977+saubhik-ghosh@users.noreply.github.com>
2023-08-11 11:12:15 +0530

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Engineer", "Front-End Developer", "Web-Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
